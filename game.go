adbf26534c0d439f13face574d08fc3b003223ff
Peter Karakas <karakaspeter@webshopexperts.hu>
2017-06-04 14:32:15 +0200

    Improve the path finding by using back tracking

--- game.go
+++ game.go
@@ -2,18 +2,192 @@ package main
 
 import (
 	"math"
+	"sort"
+	"fmt"
+	"time"
 )
 
 type Game struct {
-	data *MoveRequest
-	head Point
-	board         [][]int
+	data  *MoveRequest
+	head  Point
+	board [][]int
+}
+
+type state struct {
+	usedDirections   []string
+	currentDirection string
+	board            [][]int
+	head             Point
+}
+
+type backTrack struct {
+	directions []string
+}
+
+type sortedDirections []directionWithHeuristic
+
+type directionWithHeuristic struct {
+	direction string
+	heuristic int
+}
+
+func (sd sortedDirections) Len() int           { return len(sd) }
+func (sd sortedDirections) Swap(i, j int)      { sd[i], sd[j] = sd[j], sd[i] }
+func (sd sortedDirections) Less(i, j int) bool { return sd[i].heuristic < sd[j].heuristic }
+
+
+func (s state) DebugPrint() {
+	fmt.Println("Head: ", s.head)
+	fmt.Println("Used directions", s.usedDirections)
+	for y := 0;y < len(s.board[0]);y++ {
+		for x := 0;x < len(s.board);x++ {
+			fmt.Print(s.board[x][y], " ")
+		}
+		fmt.Println()
+	}
+}
+
+func (s *state) Clone() *state {
+	var newState state
+	//Copy board
+	board := make([][]int, len(s.board))
+	for i := 0;i < len(s.board[0]);i++ {
+		board[i] = make([]int, len(s.board[0]))
+	}
+	for x := 0;x < len(s.board);x++ {
+		for y := 0;y < len(s.board[0]);y++ {
+			board[x][y] = s.board[x][y]
+		}
+	}
+
+	newState.head = s.head
+	newState.board = board
+	newState.usedDirections = make([]string, 0)
+	newState.currentDirection = ""
+	return &newState
+}
+
+func (s *state) isDirectionApplicable(d string) bool {
+	x, y := 0, 0
+	switch d {
+	case "left":
+		x = -1
+	case "right":
+		x = 1
+	case "up":
+		y = -1
+	case "down":
+		y = 1
+	}
+	if s.head.X + x < 0 {
+		return false
+	}
+	if s.head.Y + y < 0 {
+		return false
+	}
+	if s.head.X + x > len(s.board) - 1 {
+		return false
+	}
+	if s.head.Y + y > len(s.board[0]) - 1 {
+		return false
+	}
+
+	return s.board[s.head.X + x][s.head.Y + y] == 0
+}
+
+func (s *state) ApplyDirection(d string) {
+	x, y := 0, 0
+	switch d {
+	case "left":
+		x = -1
+	case "right":
+		x = 1
+	case "up":
+		y = -1
+	case "down":
+		y = 1
+	}
+	s.head.X += x
+	s.head.Y += y
+	s.board[s.head.X][s.head.Y] = 2
+}
+
+func (s *state) RevertDirection() {
+
+}
+
+func (bt *backTrack) heuristic(from, to Point, direction string) int {
+	optimalDirection := ""
+	if dX := to.X - from.X; dX > 0 {
+		optimalDirection = "right"
+	} else if dX < 0 {
+		optimalDirection = "left"
+	}
+	if direction == optimalDirection {
+		return 0
+	}
+
+	if dY := to.Y - from.Y; dY > 0 {
+		optimalDirection = "down"
+	} else if dY < 0 {
+		optimalDirection = "up"
+	}
+
+	if optimalDirection == direction {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+func (bt *backTrack) IsFinalState(s *state, from, to Point) bool {
+	return from.X == to.X && from.Y == to.Y
+}
+
+func (bt *backTrack) FindPath(s state, from, to Point) []string {
+	newState := s.Clone()
+
+	//Find best direction which is applicable
+	//Order directions by heuristic
+	var sd sortedDirections
+	sd = make([]directionWithHeuristic, 0)
+	for _, d := range bt.directions {
+		h := bt.heuristic(from,  to, d)
+		sd = append(sd, directionWithHeuristic{d, h})
+	}
+	sort.Sort(sd)
+	i := 0
+	for len(newState.usedDirections) < 4 {
+		newState.usedDirections = append(newState.usedDirections, sd[i].direction)
+		if newState.isDirectionApplicable(sd[i].direction) {
+			newState.ApplyDirection(sd[i].direction)
+			if bt.IsFinalState(newState, newState.head, to) {
+				break
+			} else {
+				path := bt.FindPath(*newState, newState.head, to)
+				if path[0] != "" {
+					return append(path, sd[i].direction)
+				} else {
+					//Back tracking
+					newState.board[newState.head.X][newState.head.Y] = 0
+					newState.head = s.head
+				}
+			}
+		}
+		i++
+	}
+	if i < 4 {
+		return []string{sd[i].direction}
+	} else {
+		return []string{""}
+	}
+
 }
 
 func GameFactory(data *MoveRequest) *Game {
 	var head Point
 	board := make([][]int, data.Width)
-	for i := 0;i < data.Width;i++ {
+	for i := 0; i < data.Width; i++ {
 		board[i] = make([]int, data.Height)
 	}
 
@@ -36,10 +210,10 @@ func GameFactory(data *MoveRequest) *Game {
 
 func (g *Game) findClosestFood() int {
 	minIndex := 0
-	minDistance := math.Pow(float64(g.data.Width + g.data.Height), 2)
+	minDistance := math.Pow(float64(g.data.Width+g.data.Height), 2)
 
 	for i, food := range g.data.Food {
-		if d := math.Pow(float64(food.X - g.head.X), 2) + math.Pow(float64(food.Y-g.head.Y), 2); d < minDistance {
+		if d := math.Pow(float64(food.X-g.head.X), 2) + math.Pow(float64(food.Y-g.head.Y), 2); d < minDistance {
 			minDistance = d
 			minIndex = i
 		}
@@ -49,16 +223,19 @@ func (g *Game) findClosestFood() int {
 }
 
 func (g *Game) ChooseDirection() string {
+	bt := backTrack{
+		[]string{"up", "down", "left", "right"},
+	}
+	s := state{
+		[]string{},
+		"",
+		g.board,
+		g.head,
+	}
 	i := g.findClosestFood()
-	if dX := g.data.Food[i].X - g.head.X; dX > 0 {
-		return "right"
-	} else if dX < 0 {
-		return "left"
-	}
-	if dY := g.data.Food[i].Y - g.head.Y; dY > 0 {
-		return "down"
-	} else if dY < 0 {
-		return "up"
-	}
-	return "up"
+	start := time.Now()
+	path := bt.FindPath(s, g.head, g.data.Food[i])
+	fmt.Println(time.Since(start))
+
+	return path[len(path) - 1]
 }
--- game_test.go
+++ game_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,33 +17,38 @@ import (
 //	Snakes []Snake `json:"snakes"`
 //}
 
-func TestFindClosestFood(t *testing.T) {
-	//Arrange
-	activeGame := GameFactory(
-		&MoveRequest{
-			"test-id-1",
-			[]Point{{0,0}, {0,1}},
-			"test-game-id",
-			10,
-			10,
-			0,
-			[]Snake{
-				Snake{
-					[]Point{{0,8}, {0,9}},
-					100,
-					"test-id-1",
-					"test-name-1",
-					"",
-				},
-				Snake{
-					[]Point{{1, 9}, {1, 1}},
-					100,
-					"test-id-2",
-					"test-name-2",
-					"",
-				},
+func GetMoveRequestFixture() MoveRequest {
+	return MoveRequest{
+		"test-id-1",
+		[]Point{{0, 0}, {0, 1}},
+		"test-game-id",
+		10,
+		10,
+		0,
+		[]Snake{
+			Snake{
+				[]Point{{0, 8}, {0, 9}},
+				100,
+				"test-id-1",
+				"test-name-1",
+				"",
+			},
+			Snake{
+				[]Point{{2, 9}, {2, 8}},
+				100,
+				"test-id-2",
+				"test-name-2",
+				"",
 			},
 		},
+	}
+}
+
+func TestFindClosestFood(t *testing.T) {
+	//Arrange
+	moveRequest := GetMoveRequestFixture()
+	activeGame := GameFactory(
+		&moveRequest,
 	)
 
 	//Act
@@ -51,4 +57,84 @@ func TestFindClosestFood(t *testing.T) {
 	//Assert
 	assert.Equal(t, 1, index)
 
+}
+
+func TestSortedDirections(t *testing.T) {
+	sd := sortedDirections{{"left", 1}, {"right", 0}, {"up", 1}, {"down", 1}}
+	sort.Sort(sd)
+	assert.Equal(t, 0, sd[0].heuristic)
+	assert.Equal(t, "right", sd[0].direction)
+}
+
+func TestBackTrack_FindPath(t *testing.T) {
+	//Arrange
+	moveRequest := GetMoveRequestFixture()
+	moveRequest.Snakes[0].Coords = []Point{{0, 9}, {0, 8}}
+	activeGame := GameFactory(&moveRequest)
+	bt := backTrack{
+		[]string{"up", "down", "left", "right"},
+	}
+
+	//Act
+	s := state{
+		[]string{},
+		"",
+		activeGame.board,
+		activeGame.head,
+	}
+	direction := bt.FindPath(s, activeGame.head, Point{0, 1})
+
+	//Assert
+	assert.Equal(t, []string{"left", "up", "up", "up", "up", "up", "up", "up", "up", "right"}, direction)
+}
+
+func TestBackTrack_FindPath2(t *testing.T) {
+	//Arrange
+	moveRequest := GetMoveRequestFixture()
+	moveRequest.Snakes[0].Coords = []Point{{1, 0}, {0, 1}, {1, 1}}
+	moveRequest.Snakes[1].Coords = []Point{{3, 1}, {3, 2}, {3, 3}}
+	moveRequest.Food[0].X = 0
+	moveRequest.Food[0].Y = 8
+	activeGame := GameFactory(&moveRequest)
+	s := state{
+		[]string{},
+		"",
+		activeGame.board,
+		Point{1, 0},
+	}
+	bt := backTrack{
+		[]string{"up", "down", "left", "right"},
+	}
+
+	//Act
+	path := bt.FindPath(s, s.head, moveRequest.Food[0])
+
+	//Assert
+	assert.Equal(t, []string{"left", "left", "down", "down", "down", "down", "down", "down", "down", "down", "right"}, path)
+}
+
+func TestCloneState(t *testing.T) {
+	//Arrange
+	state := state{
+		[]string{"up", "down"},
+		"down",
+		[][]int{
+			{0, 0, 1, 1},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+		},
+		Point{0, 2},
+	}
+
+	//Act
+	newState := state.Clone()
+	state.head.Y = 1
+	state.board[0][1] = 1
+
+	//Assert
+	assert.Equal(t, 2, newState.head.Y)
+	assert.Equal(t, 0, newState.board[0][1])
+	assert.Equal(t, []string{}, newState.usedDirections)
+	assert.Equal(t, "", newState.currentDirection)
 }
